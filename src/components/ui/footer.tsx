b96cab095ff08f9252851038265f2eadf3153571
monkeymr014 <monkeymr014@gmail.com>
2023-11-16 12:42:44 +0100

    footer

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+export const Footer = () => {
+  return (
+    <Box bgColor='gray' as='footer' w='100%' position='fixed' bottom='0'>
+      Footer
+    </Box>
+  )
+}
