c1e87e9e8e87fc946f9cd39f2fa65c61e150d13a
monkeymr014 <monkeymr014@gmail.com>
2023-11-17 09:55:17 +0100

    Footer added Image and Link

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,9 +1,54 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <Box bgColor='gray' as='footer' w='100%' position='fixed' bottom='0'>
-      Footer
+    <Box>
+      <Flex
+        justifyContent='center'
+        flexDirection='column'
+        alignItems='center'
+        bgColor='gray'
+        as='footer'
+        w='100%'
+        py={2}
+        position='fixed'
+        bottom='0'
+      >
+        <Box pb={2}>
+          <Text>POSŁUCHAJ SOBIE NA</Text>
+        </Box>
+        <Flex>
+          <Box px={1}>
+            <Link
+              href='https://open.spotify.com/artist/3q6XSZRNzCvuTsTWdgTb4V/discography'
+              target='_blank'
+            >
+              <Image
+                src='/media/spotify.jpg'
+                width={120}
+                height={40}
+                alt='Posłuchaj Tarło na Spotify'
+              />
+            </Link>
+          </Box>
+          <Box px={1}>
+            <Link
+              href='https://music.apple.com/pl/artist/tar%C5%82o/1484447072'
+              target='_blank'
+            >
+              <Image
+                src='/media/itunes.jpg'
+                width={120}
+                height={40}
+                className='hidden md:block'
+                alt='Posłuchaj Tarło na ITunes'
+              />
+            </Link>
+          </Box>
+        </Flex>
+      </Flex>
     </Box>
   )
 }
