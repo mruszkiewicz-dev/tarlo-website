3f656e6fc241b5d942619843ae72a9ae979d1c78
monkeymr014 <monkeymr014@gmail.com>
2023-11-17 10:21:03 +0100

    add home background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+import { Box } from '@chakra-ui/react'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <Box width='100%' height='100vh' position='relative'>
+      <Image
+        src='/background_home.jpg'
+        layout='fill'
+        objectFit='cover'
+        alt='Tarło tło'
+      />
+    </Box>
+  )
 }
