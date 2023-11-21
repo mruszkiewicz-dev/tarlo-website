858443940544433fcce6ba168e890a6910d2f7a3
monkeymr014 <monkeymr014@gmail.com>
2023-11-21 15:20:18 +0100

    card

--- src/components/ui/card-member.tsx
+++ src/components/ui/card-member.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import { myFont } from '@/styles/fonts'
 import { Box, Center, Text, Image, Flex } from '@chakra-ui/react'
 import { rotateVertCenter } from '@/styles/animations'
 
 export const CardMember = () => {
   const animation = `${rotateVertCenter} 0.9s linear both`
+  const [active, setActive] = useState(false)
 
   return (
     <>
-      <Box p={2} position='relative'>
+      <Box
+        p={2}
+        position='relative'
+        onMouseEnter={() => setActive(true)}
+        onMouseLeave={() => setActive(false)}
+        _hover={{ animation: animation }}
+      >
         <Center color='white'>
           <Flex alignItems='center' justifyContent='center'>
             <Image
@@ -18,28 +26,32 @@ export const CardMember = () => {
               border='solid yinmn_blue.200'
               borderRadius='5px'
             />
-            <Box
-              position='absolute'
-              top='55%'
-              left='50%'
-              transform='translate(-50%, -50%)'
-              textAlign='center'
-            >
-              <Text
-                fontSize='24px'
-                color='white'
-                fontFamily={myFont.style.fontFamily}
+            {active && (
+              <Box
+                position='absolute'
+                top='55%'
+                left='50%'
+                transform='translate(-50%, -50%)'
+                textAlign='center'
+                background='none'
               >
-                Name
-              </Text>
-              <Text
-                fontSize='24px'
-                color='white'
-                fontFamily={myFont.style.fontFamily}
-              >
-                Name
-              </Text>
-            </Box>
+                <Text
+                  fontSize='24px'
+                  color='white'
+                  fontFamily={myFont.style.fontFamily}
+                >
+                  Name
+                </Text>
+                <Text
+                  fontSize='24px'
+                  color='white'
+                  fontFamily={myFont.style.fontFamily}
+                  background='none'
+                >
+                  Name
+                </Text>
+              </Box>
+            )}
           </Flex>
         </Center>
       </Box>
