f0f53cf4f30fa362dc699cb529c930a4e4fc07c3
monkeymr014 <monkeymr014@gmail.com>
2023-11-22 21:02:12 +0100

    card album styled

--- src/components/ui/card-album.tsx
+++ src/components/ui/card-album.tsx
@@ -14,12 +14,18 @@ interface CardAlbumProps {
 
 export const CardAlbum: React.FC<CardAlbumProps> = ({ name, id, desc }) => {
   return (
-    <Box mx={10}>
+    <Box mx={10} my={2}>
       <Flex alignItems='center' justifyContent='center' direction='column'>
         <Text fontSize={30}>{name}</Text>
         <Image src={`/albums/${id}.jpg`} width={300} height={300} alt={name} />
         {desc.map((item, index) => (
-          <Flex key={index} mt={1}>
+          <Flex
+            key={index}
+            mt={1}
+            direction={{ base: 'column', md: 'row' }}
+            alignItems='center'
+            justifyContent='center'
+          >
             <Text fontSize={20}>{item.title}:</Text>
             <Text fontSize={20} ml={1} color='red.600'>
               {item.desc}
