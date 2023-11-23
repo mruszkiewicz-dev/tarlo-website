4f12ea3afd4e6de39a1ffa262815a671a7dffdb3
monkeymr014 <monkeymr014@gmail.com>
2023-11-23 20:22:53 +0100

    add form component

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,3 +1,28 @@
+import { useFormik } from 'formik'
+import { Input } from '@chakra-ui/react'
+
 export const Form = () => {
-  return <p>aaaa</p>
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      message: '',
+    },
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <Input
+        id='email'
+        name='email'
+        type='email'
+        variant='filled'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+    </form>
+  )
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,12 @@
 import { MyText } from '@/components/ui/my-text'
 import { Flex } from '@chakra-ui/react'
-
+import { Form } from '@/components/ui/form'
 export default function Contact() {
   return (
     <>
       <MyText>Kontakt</MyText>
       <Flex mt={{ base: 2, xl: 10 }} align='center' justifyContent='center'>
-        <p>sss</p>
+        <Form />
       </Flex>
     </>
   )
