077979462332f0d8f79d4c7222f7ffb0b003f92c
monkeymr014 <monkeymr014@gmail.com>
2023-11-23 20:14:29 +0100

    install formik

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,13 @@
+import { MyText } from '@/components/ui/my-text'
+import { Flex } from '@chakra-ui/react'
+
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <>
+      <MyText>Kontakt</MyText>
+      <Flex mt={{ base: 2, xl: 10 }} align='center' justifyContent='center'>
+        <p>sss</p>
+      </Flex>
+    </>
+  )
 }
