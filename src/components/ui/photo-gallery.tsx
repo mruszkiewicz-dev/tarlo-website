810b920d277ba5cdf976251355feef9534d6d6a1
monkeymr014 <monkeymr014@gmail.com>
2024-01-18 08:11:36 +0100

    up

--- src/components/ui/photo-gallery.tsx
+++ src/components/ui/photo-gallery.tsx
@@ -1,26 +1,34 @@
-import Image from 'next/image'
-import { Box, Flex, Text } from '@chakra-ui/react'
+// PhotoGallery.tsx
 
+import { Flex } from '@chakra-ui/react'
+
+// Function to fetch data
 async function getData() {
   const res = await fetch('/api/concerts')
 
   if (!res.ok) {
-    throw new Error('Błąd')
+    throw new Error('Failed to fetch data')
   }
 
   return res.json()
 }
 
-export const PhotoGallery = () => {
+interface PhotoGalleryProps {
+  data: any // Adjust the type based on the actual structure of your data
+}
+
+const PhotoGallery: React.FC<PhotoGalleryProps> = ({ data }) => {
+  console.log(data)
+
   return (
-    <>
-      <Flex
-        w={{ base: '100%', md: '50%' }}
-        align='center'
-        justifyContent='center'
-      >
-        <Image src='/band/bartek.jpg' alt='photo' width={20} height={20} />
-      </Flex>
-    </>
+    <Flex
+      w={{ base: '100%', md: '50%' }}
+      align='center'
+      justifyContent='center'
+    >
+      {/* Display your content or images here */}
+    </Flex>
   )
 }
+
+export default PhotoGallery
