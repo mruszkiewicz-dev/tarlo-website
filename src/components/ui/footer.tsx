8f663819defc06480d26c6ad6d07d6b1a2fa915a
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2024-01-19 13:09:41 +0100

    footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,20 +1,25 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, Divider, Center } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <Box zIndex={9999}>
+    <Flex
+      zIndex={9999}
+      justifyContent='center'
+      alignItems='center'
+      as='footer'
+      w='100%'
+      bgColor='#000000'
+    >
       <Flex
         justifyContent='center'
         flexDirection='column'
         alignItems='center'
-        bgColor='#000000'
-        as='footer'
-        w='100%'
         h={20}
         py={2}
         bottom='0'
+        w={200}
       >
         <Box pb={2}>
           <Text color='white'>POSŁUCHAJ SOBIE NA</Text>
@@ -49,6 +54,66 @@ export const Footer = () => {
           </Box>
         </Flex>
       </Flex>
-    </Box>
+      <Box height='50px' px={1}>
+        <Divider orientation='vertical' colorScheme='whiteAlpha' size='10' />
+      </Box>
+      <Flex
+        justifyContent='center'
+        flexDirection='column'
+        alignItems='center'
+        h={20}
+        py={2}
+        bottom='0'
+        p={2}
+        w={200}
+      >
+        <Box pb={2}>
+          <Text color='white'>ZNAJDZIESZ NAS</Text>
+        </Box>
+        <Flex>
+          <Box px={2}>
+            <Link
+              href='https://www.instagram.com/tarlo_rockband/'
+              target='_blank'
+            >
+              <Image
+                src='/media/ins.svg'
+                width={30}
+                height={30}
+                alt='Posłuchaj Tarło na Spotify'
+              />
+            </Link>
+          </Box>
+          <Box px={2}>
+            <Link
+              href='https://www.youtube.com/channel/UCAK10cFtn2SoFgZu-TsP1Tg'
+              target='_blank'
+            >
+              <Image
+                src='/media/yt.svg'
+                width={30}
+                height={30}
+                className='hidden md:block'
+                alt='Posłuchaj Tarło na ITunes'
+              />
+            </Link>
+          </Box>
+          <Box px={2}>
+            <Link
+              href='https://www.facebook.com/tarlorockband/'
+              target='_blank'
+            >
+              <Image
+                src='/media/fb.svg'
+                width={30}
+                height={30}
+                className='hidden md:block'
+                alt='Posłuchaj Tarło na ITunes'
+              />
+            </Link>
+          </Box>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
--- src/components/ui/my-text.tsx
+++ src/components/ui/my-text.tsx
@@ -9,7 +9,7 @@ export const MyText = ({ children }: TextProps) => {
   return (
     <Center>
       <Text
-        pt={2}
+        pt={0}
         mb={{ base: 0, md: 8 }}
         fontFamily={myFont.style.fontFamily}
         fontSize={{ base: '6xl', md: '7xl' }}
