b0ba880a6b5d2e2762a20be1560db294a4327674
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2024-01-23 12:33:17 +0100

    seo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_ROBOTS_TXT: process.env.NEXT_PUBLIC_ROBOTS_TXT,
+  },
   images: {
     remotePatterns: [
       {
