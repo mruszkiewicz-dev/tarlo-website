15bd90d1f0b0042b542514aad46d959dfb15d36b
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2024-01-24 17:04:59 +0100

    hot fix

--- src/pages/concerts.tsx
+++ src/pages/concerts.tsx
@@ -1,13 +1,16 @@
 import { MyText } from '@/components/ui/my-text'
 import { Flex } from '@chakra-ui/react'
 import { ConcertList } from '@/components/ui/concert-list'
+import { Suspense } from 'react'
 
 export default function Concerts() {
   return (
     <>
       <MyText>Koncerty</MyText>
       <Flex mt={{ base: 2, xl: 10 }} align='center' justifyContent='center'>
-        <ConcertList />
+        <Suspense fallback={<p>Loading feed...</p>}>
+          <ConcertList />
+        </Suspense>
       </Flex>
     </>
   )
