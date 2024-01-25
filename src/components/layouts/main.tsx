7d316ce2ed45e80f8350a7c535405f61075ea21c
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2024-01-25 16:24:53 +0100

    seo

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -12,17 +12,33 @@ const Main: React.FC<MainProps> = ({ children }) => {
   return (
     <div>
       <Head>
+        <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content='Tarło - Oficjalna Strona Zespołu' />
+        <meta
+          name='description'
+          content='Oficjalna strona zespołu Tarło. Znajdź informacje o naszych koncertach, muzyce i najnowszych wydawnictwach.'
+        />
         <meta name='author' content='Tarło' />
-        <meta name='author' content='tarlo' />
-        <link rel='apple-touch-icon' href='apple-touch-icon.png' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <meta property='og:site_name' content='Tarło' />
-        <meta name='og:title' content='Tarło - Oficjalna Strona Zespołu' />
+        <meta property='og:title' content='Tarło - Oficjalna Strona Zespołu' />
         <meta property='og:type' content='website' />
         <meta property='og:image' content='https://www.tarlo.pl/logo.svg' />
         <title>Tarło - Oficjalna Strona Zespołu</title>
+        <meta
+          name='keywords'
+          content='Tarło, zespół muzyczny, koncerty, muzyka, nowości muzyczne'
+        />
+        <meta name='robots' content='index, follow' />
+        <meta name='googlebot' content='index, follow' />
+        <link rel='canonical' href='https://www.tarlo.pl/' />
+        <link
+          rel='sitemap'
+          type='application/xml'
+          title='Sitemap'
+          href='/sitemap.xml'
+        />
       </Head>
       <Flex direction='column' minH='100vh'>
         <NavBar />
--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,10 +1,36 @@
 import { PhotoGallery } from '@/components/ui/photo-gallery'
 import { MyText } from '@/components/ui/my-text'
 import { Flex } from '@chakra-ui/react'
+import Head from 'next/head'
 
 export default function Gallery() {
   return (
     <>
+      <Head>
+        <title>Tarło - Galeria Zdjęć Koncertowych</title>
+        <meta
+          name='description'
+          content='Przeglądaj naszą galerię zdjęć z niezapomnianych koncertów zespołu Tarło. Zobacz emocje i atmosferę naszych występów.'
+        />
+        <meta
+          name='keywords'
+          content='Tarło, galeria koncertowa, zdjęcia koncertowe, atmosfera koncertu, emocje, muzyka na żywo'
+        />
+        <meta name='robots' content='index, follow' />
+        <meta
+          property='og:title'
+          content='Tarło - Galeria Zdjęć Koncertowych'
+        />
+        <meta
+          property='og:description'
+          content='Przeglądaj naszą galerię zdjęć z niezapomnianych koncertów zespołu Tarło. Zobacz emocje i atmosferę naszych występów.'
+        />
+        <meta
+          property='og:image'
+          content='https://www.tarlo.pl/galeria-cover.jpg'
+        />
+        <link rel='canonical' href='https://www.tarlo.pl/galeria' />
+      </Head>
       <MyText>Galeria</MyText>
       <Flex mt={{ base: 2, xl: 10 }} align='center' justifyContent='center'>
         <PhotoGallery />
